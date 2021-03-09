3ef7aaa4b6b48bd9069573e1a98d5cfcae253486
Victoria Iurina <Victoria_iurina@epam.com>
2021-03-09 20:39:54 +0300

    feat: Add solution for 'Reverse Int'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+  let result;
+  let str = '';
+  let arr = [];
+  let newStr;
+  str = n + '';
+  arr = str.split('').reverse();
+  if(n < 0) {
+    arr.pop();
+  }
+  newStr = arr.join('');
+  result = newStr;
+  return +result;
 }
